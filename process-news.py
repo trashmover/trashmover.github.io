9e952e02fdac33c2cde6f078270e87efcab4d830
trashmover <>
2020-07-28 09:53:41 -0500

    update

--- process-news.py
+++ process-news.py
@@ -1,5 +1,21 @@
 f = open("news.txt", "r")
 content = f.readlines()
-for i in range(len(content)):
-    cur = content[i][len(str(i)) + 1:-1]
+clean_content = []
+
+for cur in content:
+	if cur != '\n':
+		clean_content.append(cur.strip())
+
+
+for i in range(len(clean_content)):
+    cur = clean_content[i][len(str(i)) + 1:]
     print("<li>" + cur + "</li>")
+
+
+from googletrans import Translator
+translator = Translator()
+
+for i in range(len(clean_content)):
+    cur = clean_content[i][len(str(i)) + 1:]
+    cur = translator.translate(cur)
+    print("<li>" + cur.text + "</li>")
